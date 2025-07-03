1e787244f356bfec2cef7e921750e72928749681
bensbehChaimae <ayame2420pablm@gmail.com>
2025-07-03 16:01:26 +0100

    Fourth commit

---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+# ResponseSignal inherit from Enum :
+class ResponseSignal(Enum):
+
+    FILE_VALIDATED_SUCCESS = "File validated successfully"
+
+    FILE_TYPE_NOTE_SUPPORTED = "File type not supported"
+    FILE_SIZE_EXCEEDED = "File size exceeded"
+    FILE_UPLOAD_SUCCESS = "File upload success"
+    FILE_UPLOAD_FAIL = "File upload fail"
+
+
+
+
---
+++ src/utils/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings) :
+
+    model_config = SettingsConfigDict(env_file=".env")
+    # Validation of data : 
+    APP_NAME: str
+    APP_VERSION: str
+    OPENAI_API_KEY: str 
+
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
+
+    FILE_DEFAULT_CHUNK_SIZE: int
+
+    
+def get_settings():
+    return Settings()
+
+
