e5597d2615b069277e777ac69788b2f4eb02ab80
bensbehChaimae <ayame2420pablm@gmail.com>
2025-07-11 14:43:13 +0100

    update config

--- src/utils/config.py
+++ src/utils/config.py
@@ -11,11 +11,29 @@ class Settings(BaseSettings) :
 
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
-
     FILE_DEFAULT_CHUNK_SIZE: int
 
+    # Database :
     MONGODB_URL: str
     MONGODB_DATABASE: str 
+
+    # LLMs :
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str=None
+    OPENAI_API_URL: str=None
+    COHERE_API_KEY: str=None
+
+    GENRERATION_MODEL_ID: str=None
+    EMBEDDING_MODEL_ID: str=None
+    EMBEDDING_MODEL_SIZE: int=None
+
+    INPUT_DEFAULT_MAX_CHARACTERS: int=None
+    GENERATION_DEFAULT_MAX_TOKENS: int=None
+    GENERATION_DEFAULT_TEMPERATURE: float=None
+
+
 
     
 def get_settings():
