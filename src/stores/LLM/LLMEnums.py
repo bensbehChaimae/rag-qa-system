eeb7871ff9662d21dd30f6326cece6fafa396d07
bensbehChaimae <ayame2420pablm@gmail.com>
2025-07-11 13:22:07 +0100

    Update LLMEnums

--- src/stores/LLM/LLMEnums.py
+++ src/stores/LLM/LLMEnums.py
@@ -14,6 +14,18 @@ class OpenAIEnums(Enum):
     ASSISTANT = "assistant"
 
 
-    
+class CoHereEnums(Enum):
+    SYSTEM = "SYSTEM"
+    USER = "USER"
+    ASSISTANT = "CHATBOT"
+
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
+
+
+class DocumentTypeEnum(Enum):
+    DOCUMENT = "document"
+    QUERY = "query"
+
     
 
