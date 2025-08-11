955749302b50282a111540e429d460d356b8a682
bensbehChaimae <ayame2420pablm@gmail.com>
2025-08-11 13:29:12 +0100

    edit openai client to add base url

--- src/stores/LLM/providers/OpenAIProvider.py
+++ src/stores/LLM/providers/OpenAIProvider.py
@@ -22,8 +22,8 @@ class OpenAIProvider(LLMInterface):
         self.embedding_size = None
 
         self.client = OpenAI(
-            api_key = self.api_key 
-            # base_url = self.api_url if self.api_url and len(self.api_url) else None
+            api_key = self.api_key,
+            base_url = self.api_url if self.api_url and len(self.api_url) else None
         )
 
         self.enums = OpenAIEnums
